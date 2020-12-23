d226bc85f4ddd34528beed1a4311e747feb3742d
Skayo <jonas@skayo.dev>
2020-12-23 12:45:38 +0100

    Improved utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,51 +2,32 @@ export function hasNextPan(pans, pan) {
   return pans.length - 1 > pans.indexOf(pan)
 }
 
-export const getHumanlizedTransformerName = transformer => {
-  const names = {
-    html: 'HTML',
-    pug: 'Pug',
-    markdown: 'Markdown',
-    js: 'JavaScript',
-    'vue-jsx': 'Vue JSX',
-    babel: 'Babel',
-    jsx: 'JSX', // @deprecated, use "babel"
-    css: 'CSS',
-    reason: 'Reason',
-    'coffeescript-2': 'CoffeeScript 2',
-    cssnext: 'cssnext',
-    less: 'LESS',
-    typescript: 'TypeScript',
-    sass: 'SASS',
-    scss: 'SCSS',
-    rust: 'Rust',
-    stylus: 'Stylus'
-  }
-
-  return names[transformer] || transformer
+const transformers = {
+  html:             ['HTML',           'htmlmixed',       'html'],
+  pug:              ['Pug',            'pug',             'pug'],
+  markdown:         ['Markdown',       'markdown',        'md'],
+  js:               ['JavaScript',     'jsx',             'js'],
+  'vue-jsx':        ['Vue JSX',        'jsx',             'vue'],
+  babel:            ['Babel',          'jsx',             'jsx'],
+  jsx:              ['JSX',            'jsx',             'jsx'],
+  css:              ['CSS',            'css',             'css'],
+  reason:           ['Reason',         'mllike',          're'],
+  'coffeescript-2': ['CoffeeScript 2', 'coffeescript',    'coffee'],
+  cssnext:          ['cssnext',        'css',             'pcss'],
+  less:             ['LESS',           'text/x-less',     'less'],
+  typescript:       ['TypeScript',     'text/typescript', 'ts'],
+  sass:             ['SASS',           'text/x-sass',     'sass'],
+  scss:             ['SCSS',           'text/x-scss',     'scss'],
+  rust:             ['Rust',           'rust',            'rs'],
+  stylus:           ['Stylus',         'text/x-styl',     'styl']
 }
 
-export const getEditorModeByTransfomer = transformer => {
-  const modes = {
-    html: 'htmlmixed',
-    pug: 'pug',
-    markdown: 'markdown',
-    js: 'jsx',
-    'vue-jsx': 'jsx',
-    babel: 'jsx',
-    jsx: 'jsx', // @deprecated, use "babel"
-    css: 'css',
-    reason: 'mllike',
-    'coffeescript-2': 'coffeescript',
-    cssnext: 'css',
-    less: 'text/x-less',
-    typescript: 'text/typescript',
-    sass: 'text/x-sass',
-    scss: 'text/x-scss',
-    rust: 'rust',
-    stylus: 'text/x-styl'
-  }
-  return modes[transformer]
-}
+const getHumanlizedTransformerName = transformer => transformers[transformer][0] || transformer
+
+const getEditorModeByTransfomer = transformer => transformers[transformer][1]
+
+const getFileExtByTransformer = transformer => transformers[transformer][2]
+
+const getTransformerByFileExt = fileExt => Object.keys(transformers).find(transformer => transformers[transformer][2] === fileExt)
 
 export const inIframe = window.self !== window.top
